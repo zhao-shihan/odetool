379155f660b81c96f22f1085c7af70f2105669a1
zhaoShiEnthalpy <zhao_shihan@foxmail.com>
2021-01-09 01:55:06 +0800

    Update

--- odetool.hh
+++ odetool.hh
@@ -2,96 +2,93 @@
 #define ODETOOL_H
 
 #include <vector>
+#include <omp.h>
 
-#define odelist vector
-using std::odelist;
+namespace odetool {
 
-typedef double                                         odefloat;
+    typedef double                                            odefloat;
+    typedef std::vector<odefloat>                             list_odefloat;
+    typedef std::vector<std::vector<odefloat>>                list_list_odefloat;
+    typedef std::vector<odefloat(*)(list_odefloat, odefloat)> list_odefunction;
 
-typedef odelist<odefloat(*)(list_odefloat, odefloat)>  list_odeeqn;
-typedef odelist<odefloat>                              list_odefloat;
-typedef odelist<list_odefloat>                         list_list_odefloat;
-
-typedef list_odeeqn::iterator                          it_list_odeeqn;
-typedef list_list_odefloat::iterator                   it_list_list_odefloat;
-typedef list_odefloat::iterator                        it_list_odefloat;
-typedef odelist<it_list_odefloat>                      list_it_list_odefloat;
-typedef odelist<it_list_odefloat>::iterator            it_list_it_list_odefloat;
-
-class ODEtool {
-private:
-    list_odeeqn eqn_;
-    it_list_odeeqn it_eqn_;
-
-    size_t n;
-
-    list_odefloat list_x_;
-    it_list_odefloat it_list_x_;
-
-    list_list_odefloat list_list_y_;
-    it_list_list_odefloat it_list_list_y_;
-    list_it_list_odefloat list_it_list_y_;
-    it_list_it_list_odefloat it_list_it_list_y_;
-
-    list_list_odefloat list_list_k_;
-    it_list_list_odefloat it_list_list_k_;
-    list_it_list_odefloat list_it_list_k_;
-    it_list_it_list_odefloat it_list_it_list_k_;
-
-public:
-    ODEtool(const list_odeeqn& eqn, odefloat x_begin, odefloat x_end, const odefloat& h);
-    ~ODEtool();
-
-    void RK45(const list_odefloat& init_cond);
-};
-
-ODEtool::ODEtool(const list_odeeqn& eqn, odefloat x_begin, odefloat x_end, const odefloat& h) :
-    eqn_(eqn),
-    it_eqn_(eqn_.begin()),
-    list_list_y_(eqn.size()),
-    it_list_list_y_(list_list_y_.begin()),
-    list_it_list_y_(eqn.size()),
-    it_list_it_list_y_(list_it_list_y_.begin()),
-    list_list_k_(eqn.size()),
-    it_list_list_k_(list_list_k_.begin()) {
-    if (x_begin > x_end) {
-        odefloat temp = x_begin;
-        x_begin = x_end;
-        x_end = temp;
-    }
-    n = (x_end - x_begin) / h + 1;
-
-    list_x_.resize(n);
-    it_list_x_ = list_x_.begin();
-    *it_list_x_ = x_begin;
-    for (++it_list_x_; it_list_x_ != list_x_.end(); ++it_list_x_) {
-        *it_list_x_ = *(it_list_x_ - 1) + h;
-    }
-    for (; it_list_list_y_ != list_list_y_.end(); ++it_list_list_y_) {
-        it_list_list_y_->resize(n);
-        *it_list_it_list_y_ = it_list_list_y_->begin();
-        ++it_list_it_list_y_;
-    }
-}
-
-ODEtool::~ODEtool() {}
-
-void ODEtool::RK45(const list_odefloat& init_cond) {
-    list_it_list_k_.resize(4);
-    it_list_it_list_k_ = list_it_list_k_.begin();
-    for (it_list_list_k_ = list_list_k_.begin(); it_list_list_k_ != list_list_k_.end(); ++it_list_list_k_) {
-        it_list_list_k_->resize(4);
-    }
-    list_odefloat::const_iterator it_init_cond = init_cond.begin();
-    for (it_list_it_list_y_ = list_it_list_y_.begin(); it_list_it_list_y_ != list_it_list_y_.end();
-        ++it_list_it_list_y_) {
-        **it_list_it_list_y_ = *it_init_cond;
-        ++it_init_cond;
+    inline list_odefloat DiscretizeX(const odefloat& x_begin, const odefloat& x_end, const odefloat& h) {
+        size_t n = (x_end - x_begin) / h + 1;
+        list_odefloat x(n);
+#pragma omp parallel for
+        for (size_t i = 0; i < n; ++i) {
+            x[i] = x_begin + i * h;
+        }
+        return x;
     }
 
-    for (it_list_x_ = list_x_.begin(); it_list_x_ != list_x_.end(); ++it_list_x_) {
-
+    inline list_list_odefloat DiscretizeY(const size_t& eqn_size, const size_t& x_size) {
+        list_list_odefloat y(eqn_size);
+#pragma omp parallel for
+        for (list_list_odefloat::iterator it = y.begin(); it != y.end(); ++it) {
+            it->resize(x_size);
+        }
+        return y;
     }
-}
+
+    void RK45(const list_odefunction& eqn, const list_odefloat& x, list_list_odefloat& y) {
+        list_list_odefloat k(4);
+#pragma omp parallel for
+        for (list_list_odefloat::iterator it = k.begin(); it != k.end(); ++it) {
+            it->resize(eqn.size());
+        }
+        list_list_odefloat temp_y(4);
+#pragma omp parallel for
+        for (list_list_odefloat::iterator it = temp_y.begin(); it != temp_y.end(); ++it) {
+            it->resize(eqn.size());
+        }
+
+        for (size_t n = 0; n < x.size() - 1; ++n) {
+            odefloat h = x[n + 1] - x[n];
+
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                temp_y[0][i] = y[i][n];
+            }
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                k[0][i] = eqn[i](temp_y[0], x[n]) * h;
+            }
+
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                temp_y[1][i] = temp_y[0][i] + 0.5 * k[0][i];
+            }
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                k[1][i] = eqn[i](temp_y[1], x[n] + 0.5 * h) * h;
+            }
+
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                temp_y[2][i] = temp_y[0][i] + 0.5 * k[1][i];
+            }
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                k[2][i] = eqn[i](temp_y[2], x[n] + 0.5 * h) * h;
+            }
+
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                temp_y[3][i] = temp_y[0][i] + k[2][i];
+            }
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                k[3][i] = eqn[i](temp_y[3], x[n] + h) * h;
+            }
+
+#pragma omp parallel for
+            for (size_t i = 0; i < eqn.size(); ++i) {
+                y[i][n + 1] = y[i][n] + 0.166666666666667 * (k[0][i] + 2.0 * k[1][i] + 2.0 * k[2][i] + k[3][i]);
+            }
+        }
+    }
+
+} // namespace odetool
 
 #endif // ODETOOL_H
+
--- test/test.cc
+++ test/test.cc
@@ -1,8 +1,28 @@
+#include <fstream>
+#include <cmath>
 #include "odetool.hh"
+using namespace odetool;
+
+inline odefloat f(list_odefloat y, odefloat x) {
+    return -sin(y[0]);
+}
 
 int main(int argc, char const* argv[]) {
-    list_odeeqn eqn(3);
-    ODEtool ode45(eqn, (odefloat)0.1, (odefloat)1.0, (odefloat)0.01);
+    list_odefunction eqn{ f };
+    list_odefloat x = DiscretizeX(0, 10, 0.01);
+    list_list_odefloat y = DiscretizeY(1, x.size());
+    y[0][0] = 1;
+    RK45(eqn, x, y);
+
+    std::ofstream fout("result.csv");
+    if (fout.is_open()) {
+        fout << "x,y\n";
+        for (size_t i = 0; i < x.size(); ++i) {
+            fout << x[i] << ',' << y[0][i] << std::endl;
+        }
+        fout.close();
+    }
+
     return 0;
 }
 
